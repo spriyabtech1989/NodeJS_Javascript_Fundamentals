08218c811986a62ffabed7215af30cfa39792673
spriyabtech1989 <s.priyabtech1989@gmail.com>
2019-09-03 22:06:31 +0530

    new filesadded

---
+++ Pgm27_ArrowFunctions.js
@@ -0,0 +1,15 @@
+//Arrow Funcion.. Many different way in javascript
+
+
+//simple
+
+let hi = () => { console.log("Hi Priya");}
+
+hi();
+
+//input paramenter
+
+let myFunction = (name) => { console.log(`Priya ${name}`)};
+//passed the name parameter
+
+myFunction('radha welcome');
