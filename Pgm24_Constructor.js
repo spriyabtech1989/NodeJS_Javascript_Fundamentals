39c684bd8f55e90d168bbea39a7b7c25c5250700
spriyabtech1989 <s.priyabtech1989@gmail.com>
2019-09-03 14:13:23 +0530

    new files committed

--- Pgm24_Constructor.js
+++ Pgm24_Constructor.js
@@ -6,6 +6,15 @@ function Car(make,model,year) {
     this.year = year;
 }
 
-let myCar = new Car('TVS', ' 123456', 2018);
+let myCar = new Car('TVS', ' 123456', 2018); //new keyword is a any function
 console.log(myCar);
+
+
+//Object
+function myFunction() {
+    console.log('I am a Simple function');
+}
+
+let newMyfunction = new myFunction(); //Object
+console.log(typeof newMyfunction);
 
---
+++ Pgm25_Prototypes.js
@@ -0,0 +1,45 @@
+//Javascript is an objects,
+//called the name is Class
+//All the properties parant class from child class. 
+//Instance the object
+//A create a instance on object
+//Relationshp b/w classes and objects
+//inheritance
+//oops concept
+//Protype and Inheritance
+//objects linkin other objects
+
+//Object
+let secondHandBike = {
+    make: 'tvs',
+    model: 'old',
+    oldBike: 'yes',
+    year: 2018,
+    ownerName: 'solai'
+};
+
+//New object based on Existing Objects
+let newBike = Object.create(secondHandBike);//Prototype is a create the instance of the object
+console.log(newBike.ownerName); // get the value from secondhandbike
+
+//we have 2 objects. we need to change the exisiting property
+
+console.log(Object.getPrototypeOf(newBike)); //values in newBike... getPrototypeOf
+
+let myPrototype = Object.getPrototypeOf(newBike);
+console.log(myPrototype.ownerName);
+
+secondHandBike.ownerWifeName = 'priya'; //Additional property and value will be added
+console.log(newBike.ownerWifeName);
+
+
+//hasOwnProperty Method
+
+console.log(secondHandBike.hasOwnProperty('year')); //To check the hasOwnproprty values
+
+//doubt
+
+
+console.log(secondHandBike.hasOwnProperty('model')); //To check the hasOwnproprty values
+
+console.log(newBike.hasOwnProperty('model')); //To check the hasOwnproprty values
